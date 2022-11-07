63bf827f6478cc32bb4710ede6fcdd44b0fa6397
FrancescoXD <28204054+FrancescoXD@users.noreply.github.com>
2022-11-07 15:39:06 +0100

    moved updated code to root folder

--- client.py
+++ client.py
@@ -1,14 +1,43 @@
 import socket
+import threading
 import sys
 
-HOST = sys.argv[1]
-PORT = int(sys.argv[2])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    data_sent = input("Write some text to send to the server: ")
-    #s.sendall(data_sent.encode())
-    s.send(data_sent.encode())
-    data = s.recv(2048)
+def recv_msg():
+    while True:
+        recv_msg = s.recv(2048)
+        if not recv_msg:
+            sys.exit()
+        print(recv_msg.decode())
 
-print(f"Data received from server: {data}")
+
+def send_msg(username, sock):
+    try:
+        while True:
+            send_msg = input()
+            if send_msg != '':
+                send_msg = username + ': ' + send_msg
+                s.send(send_msg.encode())
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt, closing...')
+    finally:
+        sock.close()
+        sys.exit()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f'Usage: {sys.argv[0]} <HOST> <PORT>')
+    else:
+        username = input('Username: ')
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((sys.argv[1], int(sys.argv[2])))
+        print("Connected to the server!")
+        s.sendall(username.encode())
+
+        t = threading.Thread(target=recv_msg)
+        t.start()
+
+        # event loop
+        send_msg(username, s)
--- server.py
+++ server.py
@@ -3,50 +3,52 @@ import socket
 import selectors
 import types
 
-sel = selectors.DefaultSelector()  # efficient I/O multiplexing
+sel = selectors.DefaultSelector()
+
+SOCKETS_LIST = []
+
+
+def broadcast_new_join(sock):
+    username = sock.recv(2048)
+    join_msg = f'{username.decode()} joined the server'
+    print(join_msg)
+    for client in SOCKETS_LIST:
+        if client != sock:
+            client.send(join_msg.encode())
 
 
 def accept_wrapper(sock):
     conn, addr = sock.accept()
     print(f"Accepted new client {addr[0]}:{addr[1]}")
+    SOCKETS_LIST.append(conn)
+    broadcast_new_join(conn)
     conn.setblocking(False)
-    # qui si crea un oggetto in cui includere i dati per tutta la vita del socket
-    # questo perché si deve sapere quando il client è pronto per leggere e scrivere
-    # tutti e due gli eventi sono impostati tramite l'operatore bitwise OR
     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
     events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    # registro il socket
     sel.register(conn, events, data=data)
 
 
 def service_connection(key, mask):
-    # socket
     sock = key.fileobj
-    # data
     data = key.data
-    # mask & selectors.EVENT_READ ritorna TRUE se il socket è pronto per leggere
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(2048)
         print(f"Received {recv_data} from {data.addr[0]}:{data.addr[1]}")
-        # aggiungo i dati a data.outb da inviare successivamente
         if recv_data:
             data.outb += recv_data
-        # se non riceviamo dati il client ha chiuso il suo socket
-        # e quindi anche il server dovrebbe chiuderlo
         else:
             print(f"Closing connection to {data.addr[0]}:{data.addr[1]}")
-            # eliminiamo la registrazione in modo che non venga più monitorato
+            if sock in SOCKETS_LIST:
+                SOCKETS_LIST.remove(sock)
             sel.unregister(sock)
             sock.close()
-    # se il socket è pronto per scrivere
-    # invio data.outb al client
     if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Sending {data.outb!r} to {data.addr[0]}:{data.addr[1]}")
-            # sent contiene il numero di byte inviati (ritornato da send())
-            sent = sock.send(data.outb)
-            # è un numero che non mi serve, quindi uso la notazione slice
-            # per eliminarlo
+        if data.outb and len(SOCKETS_LIST) != 1:
+            print(f"Sending {data.outb!r} to all clients")
+            sent = 0
+            for client in SOCKETS_LIST:
+                if client != sock:
+                    sent = client.send(data.outb)
             data.outb = data.outb[sent:]
 
 
@@ -55,31 +57,23 @@ def main(HOST, PORT):
     lsock.bind((HOST, PORT))
     lsock.listen()
     print(f"Listening on {HOST}:{PORT}")
-    # non blocking mode
     lsock.setblocking(False)
-    # registra il socket da monitorare con sel.select() per gli eventi a cui si è interessati.
-    # Per il socket in ascolto, si vogliono leggere gli eventi: selectors.EVENT_READ
     sel.register(lsock, selectors.EVENT_READ, data=None)
 
     try:
         while True:
-            # select(timeout=None) ferma tutto finché non ci sono socket pronti per l'I/O
-            # ritorna una tupla per ogni socket contenente una key ed una mask
-            # es: key.fileobj è un socket
             events = sel.select(timeout=None)
             for key, mask in events:
-                # se key.data è None bisogna prima accettare il socket
-                # chiamo accept_wrapper() in modo da accettare la connessione e
-                # registrarla con il selector
                 if key.data is None:
                     accept_wrapper(key.fileobj)
-                # in questo caso il client è stato già accettato
                 else:
                     service_connection(key, mask)
     except KeyboardInterrupt:
         print("Closed by KeyboardInterrupt")
+        lsock.close()
     finally:
         sel.close()
+        lsock.close()
 
 
 if __name__ == "__main__":
